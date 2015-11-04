a50c84ba50e052b0112c479f9b433ef5bdc87504
hgm1284 <hygomezm@est.utn.ac.cr>
2015-11-04 00:23:25 -0600

    nineth
    Se colocó el editor de texto al html de crear nuevo correo.
    Se validó que se escribiera un formato de correo en el inicio de sesión
    y en el registro de un nuevo correo, al crear un nuevo nombre de
    usuario. Esto con el uso de  funciones en un .js
    La validación del formato de correo en el destinatario no funciona, sin
    embargo se dejó la función en el .js
    Se guardan los archivos de imagen, .js y .css en carpetas separadas.

---
+++ js/name.js
@@ -0,0 +1,41 @@
+/*Funcion para validar formato de correo al iniciar sesión*/
+function ValidateEmail(inputText)
+{
+    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if(inputText.value.match(mailformat))
+    {
+        window.location.href='InicioSalida.html';
+    }
+    else
+    {
+        alert("Nombre de Correo No Valido");
+    }
+}
+
+/*Funcion para validar correo en el registro al crear un nuevo nombre de usuario*/
+function ValidateRegistro(inputText)
+{
+    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if(inputText.value.match(mailformat))
+    {
+        window.location.href='Signup.html';
+    }
+    else
+    {
+        alert("Formato de Correo No Valido");
+    }
+}
+
+/*Previsto para utilizar en el destinatario*/
+function ValidateDestino(inputText)
+{
+    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if(inputText.value.match(mailformat))
+    {
+        window.location.href='SalidaClick.html';
+    }
+    else
+    {
+        alert("Formato de Correo No Valido");
+    }
+}
